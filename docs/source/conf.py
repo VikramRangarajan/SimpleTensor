fb91f53316a864f8f3a04359ff6e3b3fa0c3749f
Vikram Rangarajan <vikram.rangaraja@gmail.com>
2024-03-06 13:27:05 -0500

    Fixed docs import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@ import os
 import sys
 import inspect
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # Configuration file for the Sphinx documentation builder.
 #
