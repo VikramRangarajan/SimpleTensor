Added switchable array backend; Fixed backpropagation axes; Finished mnist_train without CNN

src/simpletensor/functions.py
@@ -4,141 +4,142 @@ from .tensor import astensor, np
 def add(a1, a2):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.__add__`
-    """    
+    """
     return astensor(a1) + a2
 
 
 def subtract(a1, a2):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.__sub__`
-    """    
+    """
     return astensor(a1) - a2
 
 
 def multiply(a1, a2):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.__mul__`
-    """    
+    """
     return astensor(a1) * a2
 
 
 def matmul(a1, a2):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.__matmul__`
-    """    
+    """
     return astensor(a1) @ a2
 
 
 def power(a1, a2):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.__pow__`
-    """    
+    """
     return astensor(a1) ** a2
 
 
 def divide(a1, a2):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.__truediv__`
-    """    
+    """
     return astensor(a1) / a2
 
 
 def sum(a, axis=None, keepdims=False):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.sum`
-    """    
+    """
     return astensor(a).sum(axis=axis, keepdims=keepdims)
 
 
 def mean(a, axis=None, keepdims=False):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.mean`
-    """    
+    """
     return astensor(a).mean(axis=axis, keepdims=keepdims)
 
 
 def std(a, axis=None, ddof=0, keepdims=False):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.std`
-    """    
+    """
     return astensor(a).std(axis=axis, ddof=ddof, keepdims=keepdims)
 
 
 def var(a, axis=None, ddof=0, keepdims=False):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.var`
-    """    
+    """
     return astensor(a).var(axis=axis, ddof=ddof, keepdims=keepdims)
 
 
 def max(a, axis=None, keepdims=False):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.max`
-    """    
+    """
     return astensor(a).max(axis=axis, keepdims=keepdims)
 
 
 def min(a, axis=None, keepdims=False):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.min`
-    """    
+    """
     return astensor(a).min(axis=axis, keepdims=keepdims)
 
 
 def exp(a):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.exp`
-    """    
+    """
     return astensor(a).exp()
 
 
 def logn(a, n=np.e):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.logn`
-    """    
+    """
     return astensor(a).logn(n=n)
 
 
 def transpose(a, axis=None):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.transpose`
-    """    
+    """
     return astensor(a).transpose(axis=axis)
 
 
 def squeeze(a, axis=None):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.squeeze`
-    """    
+    """
     return astensor(a).squeeze(axis=axis)
 
 
 def flip(a, axis=None):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.flip`
-    """    
+    """
     return astensor(a).flip(axis=axis)
 
 
 def expand_dims(a, axis=None):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.expand_dims`
-    """    
+    """
     return astensor(a).expand_dims(axis=axis)
 
 
 def reshape(a, shape=None):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.reshape`
-    """    
+    """
     return astensor(a).reshape(shape=shape)
 
 
 def relu(a):
     """
     Helper function for :py:mod:`~simpletensor.tensor.Tensor.relu`
-    """    
+    """
     return astensor(a).relu()
+
 
 def softmax(a, axis=None):
     """
@@ -155,8 +156,31 @@ def softmax(a, axis=None):
     -------
     Tensor
         Output of softmax
-    """ 
+    """
     a = astensor(a)
     a_max = a.max(axis=axis, keepdims=True)
     exp_a = (a - a_max).exp()
     return exp_a / exp_a.sum(axis=axis, keepdims=True)
+
+
+
+def categorical_cross_entropy(y_true, y_pred):
+    r"""
+    Log loss / cross entropy loss over a batch. Defined as:
+
+    .. math::
+        \text{categorical_cross_entropy}(Y, \hat Y) = -\frac{1}{\text{batch_size}}\sum_{b=1}^{\text{batch_size}} \sum_{i=1}^n Y[b, i] \log_{b}(\hat Y[b, i])
+
+    Parameters
+    ----------
+    y_true : Tensor
+        2D Tensor of shape (batch_size, # features)
+    y_pred : Tensor
+        2D Tensor of shape (batch_size, # features)
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    return -((y_true * y_pred.logn()).sum(axis=1).mean())

src/simpletensor/mnist_train.py
@@ -1,5 +1,4 @@
-from simpletensor import Tensor, softmax
-import numpy as np
+from simpletensor import Tensor, softmax, categorical_cross_entropy, np
 from urllib.request import urlretrieve
 import os
 import importlib
@@ -38,7 +37,7 @@ def parse_args():
     parser.add_argument(
         "--lr",
         type=float,
-        default=0.001,
+        default=0.01,
         help="learning rate of optimizer",
     )
     parser.add_argument(
@@ -103,10 +102,10 @@ def load_data(location):
     """
     data = np.load(location)
     return (
-        data["x_train"].reshape((-1, 784)) / 255.0,
+        data["x_train"].reshape((-1, 784)).astype("float32") / 255.0,
         data["y_train"],
     ), (
-        data["x_test"].reshape((-1, 784)) / 255.0,
+        data["x_test"].reshape((-1, 784)).astype("float32") / 255.0,
         data["y_test"],
     )
 
@@ -120,14 +119,20 @@ def main():
     data_loc = os.path.join(PATH, "mnist.npz")
 
     download_mnist(data_loc)
-    (x_train, y_train_categorical), (x_test, y_test_categorical) = load_data(data_loc)
+    (X_train, y_train_categorical), (X_test, y_test_categorical) = load_data(data_loc)
 
     # Convert to one-hot encoded vectors
-    y_train = np.zeros((60000, 10))
-    y_test = np.zeros((10000, 10))
+    y_train = np.zeros((60000, 10), dtype="float64")
+    y_test = np.zeros((10000, 10), dtype="float64")
     y_train[range(60000), y_train_categorical] = 1
     y_test[range(10000), y_test_categorical] = 1
     m = Model(dense_neurons=DENSE_NEURONS, conv_filters=CONV_FILTERS, lr=LR)
+    m.fit(
+        train_data=(X_train, y_train),
+        test_data=(X_test, y_test),
+        epochs=10,
+        batch_size=32,
+    )
     # TODO: Train model
     os.remove("mnist.npz")
 
@@ -157,14 +162,62 @@ class Model:
         b2_rand = self.rng.normal(0, 1 / 784**0.5, 10)
         self.b2 = Tensor(b2_rand)
 
+        # Trainable parameters
+        self.parameters = [self.W1, self.b1, self.W2, self.b2]
+
     def __call__(self, batch):
         Y = Tensor(batch)
         Y = Y @ self.W1 + self.b1
         Y = Y.relu()
         Y = Y @ self.W2 + self.b2
-        Y = softmax(Y, axis=1)
-
+        Y = softmax(Y, axis=(1,))
         return Y
+
+    def fit(self, train_data, test_data, epochs, batch_size):
+        X_train, y_train = train_data
+        X_test, y_test = test_data
+        for epoch in range(epochs):
+            print(f"Epoch {epoch+1} / {epochs}")
+            for phase in ["train", "val"]:
+                epoch_loss = 0
+                epoch_acc = 0
+                if phase == "train":
+                    X = X_train
+                    y = y_train
+                else:
+                    X = X_test
+                    y = y_test
+                progbar = tqdm.tqdm(range(0, len(X), batch_size), mininterval=0.25)
+                for start_index in progbar:
+                    batch_inp = X[start_index : start_index + batch_size]
+                    batch_labels = y[start_index : start_index + batch_size]
+
+                    preds = self(batch_inp)
+                    batch_loss = categorical_cross_entropy(Tensor(batch_labels), preds)
+
+                    preds_categorical = preds._array.argmax(1)
+                    labels_categorical = batch_labels.argmax(1)
+                    correct = (preds_categorical == labels_categorical).sum()
+                    epoch_acc += correct
+                    epoch_loss += batch_loss._array.item()
+
+                    progbar.set_postfix(
+                        {
+                            f"{phase}_loss": f"{epoch_loss / (batch_size + start_index):.4f}",
+                            f"{phase}_accuracy": f"{epoch_acc / (batch_size + start_index):.4f}",
+                        },
+                        refresh=False,
+                    )
+
+                    if phase == "train":
+                        # Optimization, the WHOLE point of this automatic differentiation library
+                        batch_loss.backward()  # !!!!
+                        for param in self.parameters:
+                            # Stochastic Gradient Descent
+                            param._array -= self.lr * param.grad
+                epoch_loss /= len(X)
+                epoch_acc /= len(X)
+                print(f"{phase}_loss: {epoch_loss}, {phase}_acc: {epoch_acc}")
 
 
 if __name__ == "__main__":

src/simpletensor/tensor.py
@@ -1,16 +1,5 @@
-from . import USE_GPU
-import numpy as np
 from .operation import Op
-from importlib import import_module
-
-if USE_GPU:
-    np = import_module("cupy")
-    scipy = import_module("cupyx.scipy")
-else:
-    import numpy as np
-    import scipy
-
-fftconvolve = scipy.signal.fftconvolve
+from simpletensor import np
 
 
 class Tensor:
@@ -152,11 +141,15 @@ class Tensor:
         if Tensor.grad_enabled:
             res._op = Op.ADD
             res._parents = [self, other]
-            op_axes = _broadcasted_axes(self, other)
+            op_axes = _broadcasted_axes(self.shape, other.shape)
 
             def _backward():
-                self.grad += np.sum(res.grad, axis=op_axes[0])
-                other.grad += np.sum(res.grad, axis=op_axes[1])
+                self.grad += np.sum(res.grad, axis=op_axes[0][1], keepdims=True).sum(
+                    op_axes[0][0]
+                )
+                other.grad += np.sum(res.grad, axis=op_axes[1][1], keepdims=True).sum(
+                    op_axes[1][0]
+                )
 
             res._backward = _backward
         return res
@@ -178,11 +171,15 @@ class Tensor:
         if Tensor.grad_enabled:
             res._op = Op.SUB
             res._parents = [self, other]
-            op_axes = _broadcasted_axes(self, other)
+            op_axes = _broadcasted_axes(self.shape, other.shape)
 
             def _backward():
-                self.grad += np.sum(res.grad, axis=op_axes[0])
-                other.grad -= np.sum(res.grad, axis=op_axes[1])
+                self.grad += np.sum(res.grad, axis=op_axes[0][1], keepdims=True).sum(
+                    op_axes[0][0]
+                )
+                other.grad -= np.sum(res.grad, axis=op_axes[1][1], keepdims=True).sum(
+                    op_axes[1][0]
+                )
 
             res._backward = _backward
         return res
@@ -206,11 +203,15 @@ class Tensor:
         if Tensor.grad_enabled:
             res._op = Op.MUL
             res._parents = [self, other]
-            op_axes = _broadcasted_axes(self, other)
+            op_axes = _broadcasted_axes(self.shape, other.shape)
 
             def _backward():
-                self.grad += np.sum(res.grad * other._array, axis=op_axes[0])
-                other.grad += np.sum(res.grad * self._array, axis=op_axes[1])
+                self.grad += np.sum(
+                    res.grad * other._array, axis=op_axes[0][1], keepdims=True
+                ).sum(op_axes[0][0])
+                other.grad += np.sum(
+                    res.grad * self._array, axis=op_axes[1][1], keepdims=True
+                ).sum(op_axes[1][0])
 
             res._backward = _backward
         return res
@@ -261,44 +262,38 @@ class Tensor:
             else:
                 if prepended:
                     # 1D Tensor @ >=2D Tensor
-                    op_axes = _broadcasted_axes(np.array(0), other[..., 0, 0])
+                    op_axes = _broadcasted_axes((), other.shape[:-2])
 
                     def _backward():
                         self.grad += (
                             (res.grad[..., None, :] @ other._array.swapaxes(-1, -2))
-                            .sum(op_axes[0])
+                            .sum(op_axes[0][0])
                             .squeeze(-2)
                         )
-                        other.grad += (
-                            self._array[:, None] @ res.grad[..., None, :]
-                        ).sum(op_axes[1])
+                        other.grad += self._array[:, None] @ res.grad[..., None, :]
 
                 elif appended:
                     # >=2D Tensor @ 1D Tensor
-                    op_axes = _broadcasted_axes(self._array[..., 0, 0], np.array(0))
+                    op_axes = _broadcasted_axes(self.shape[:-2], ())
 
                     def _backward():
-                        self.grad += (
-                            res.grad[..., None] @ other._array[:, None].T
-                        ).sum(op_axes[0])
+                        self.grad += res.grad[..., None] @ other._array[:, None].T
                         other.grad += (
                             (self._array.swapaxes(-1, -2) @ res.grad[..., None])
-                            .sum(op_axes[1])
+                            .sum(op_axes[1][0])
                             .squeeze(-1)
                         )
 
                 else:
                     # >2D Tensor @ >2D Tensor
-                    op_axes = _broadcasted_axes(
-                        self._array[..., 0, 0], other[..., 0, 0]
-                    )
+                    op_axes = _broadcasted_axes(self.shape[:-2], other.shape[:-2])
 
                     def _backward():
                         self.grad += (res.grad @ other._array.swapaxes(-1, -2)).sum(
-                            op_axes[0]
+                            op_axes[0][0] + op_axes[0][1]
                         )
                         other.grad += (self._array.swapaxes(-1, -2) @ res.grad).sum(
-                            op_axes[1]
+                            op_axes[1][0] + op_axes[1][1]
                         )
 
             res._backward = _backward
@@ -329,17 +324,19 @@ class Tensor:
         if Tensor.grad_enabled:
             res._op = Op.POW
             res._parents = [self, other]
-            op_axes = _broadcasted_axes(self, other)
+            op_axes = _broadcasted_axes(self.shape, other.shape)
 
             def _backward():
                 self.grad += np.sum(
-                    res.grad * other._array * self._array ** (other._array - 1),
-                    axis=op_axes[0],
-                )
+                    res.grad * other._array * self._array ** (other._array - 1.0),
+                    axis=op_axes[0][1],
+                    keepdims=True,
+                ).sum(op_axes[0][0])
                 other.grad += np.sum(
                     res.grad * res._array * np.log(self._array),
-                    axis=op_axes[1],
-                )
+                    axis=op_axes[1][1],
+                    keepdims=True,
+                ).sum(op_axes[1][0])
 
             res._backward = _backward
         return res
@@ -365,10 +362,10 @@ class Tensor:
         Tensor
             Result tensor of A / B
         """
-        return self * other**-1
+        return self * other**-1.0
 
     def __rtruediv__(self, other):
-        return self**-1 * other
+        return self**-1.0 * other
 
     __itruediv__ = __truediv__
 
@@ -631,7 +628,7 @@ class Tensor:
         self._array[index] = values
 
     def __neg__(self):
-        return self * -1
+        return self * -1.0
 
     def __pos__(self):
         return self
@@ -659,7 +656,7 @@ class Tensor:
                 if keepdims:
                     self.grad += res.grad
                 else:
-                    notnullaxis: tuple = axis or tuple(range(self.ndim - 1))
+                    notnullaxis: tuple = axis or tuple(range(self.ndim))
                     self.grad += np.expand_dims(res.grad, notnullaxis)
 
             res._backward = _backward
@@ -705,7 +702,7 @@ class Tensor:
         if axis is not None:
             df = float(np.prod([self.shape[i] for i in axis]) - ddof)
         return (
-            ((self - self.mean(axis, keepdims=keepdims)) ** 2).sum(
+            ((self - self.mean(axis, keepdims=keepdims)) ** 2.0).sum(
                 axis, keepdims=keepdims
             )
             / df
@@ -731,7 +728,7 @@ class Tensor:
         df = float(self.size - ddof)
         if axis is not None:
             df = float(np.prod([self.shape[i] for i in axis]) - ddof)
-        return ((self - self.mean(axis, keepdims=keepdims)) ** 2).sum(
+        return ((self - self.mean(axis, keepdims=keepdims)) ** 2.0).sum(
             axis, keepdims=keepdims
         ) / df
 
@@ -808,18 +805,18 @@ def astensor(a, dtype=None):
         return Tensor(a, dtype)
 
 
-def _padded_broadcast_shapes(*a, max_ndim=None):
-    return [(0,) * (max_ndim - arr.ndim) + arr.shape for arr in a]
+def _padded_broadcast_shapes(*shapes, max_ndim=None):
+    return [(0,) * (max_ndim - len(shape)) + shape for shape in shapes]
 
 
-def _broadcasted_axes(*a):
-    res_shape = np.broadcast_shapes(*(arr.shape for arr in a))
-    arr_pshapes = _padded_broadcast_shapes(*a, max_ndim=len(res_shape))
-    axes = [() for _ in a]
+def _broadcasted_axes(*shapes):
+    res_shape = np.broadcast_shapes(*shapes)
+    arr_pshapes = _padded_broadcast_shapes(*shapes, max_ndim=len(res_shape))
+    axes = [[(), ()] for _ in shapes]
     for axis in range(0, len(res_shape)):
-        for arr_num in range(len(a)):
-            if arr_pshapes[arr_num][axis] == 0 or (
-                arr_pshapes[arr_num][axis] == 1 and res_shape[axis] != 1
-            ):
-                axes[arr_num] = axes[arr_num] + (axis,)
+        for arr_num in range(len(shapes)):
+            if arr_pshapes[arr_num][axis] == 0:
+                axes[arr_num][0] = axes[arr_num][0] + (axis,)
+            elif arr_pshapes[arr_num][axis] == 1 and res_shape[axis] != 1:
+                axes[arr_num][1] = axes[arr_num][1] + (axis,)
     return axes